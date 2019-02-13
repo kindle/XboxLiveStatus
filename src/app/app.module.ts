cf4647d9858f51ad6b9432e6b50f2a8c52789c7d
bailin wei <weibailin@hotmail.com>
2019-02-13 17:34:55 +0800

    add app-update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { AppUpdate } from '@ionic-native/app-update/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,7 +19,9 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AppVersion,
+    AppUpdate,
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,60 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular'; 
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { AppUpdate } from '@ionic-native/app-update/ngx';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+
+  version: String;
+
+  constructor(
+    private appVersion: AppVersion,
+    private appUpdate: AppUpdate,
+    private platform: Platform,
+  ){
+    this.getVersionNumber().then(version => {
+      this.version = version;
+    });
+  }
+
+  getVersionNumber(): Promise<string> {
+      return new Promise((resolve) => {
+          this.appVersion.getVersionNumber().then((value: string) => {
+              resolve(value);
+          }).catch(err => {
+              console.log('getVersionNumber:' + err);
+          });
+      });
+  }
+
+  checkUpdate() {
+    const updateUrl = 'https://reddah.com/apk/livestatus/update.xml';
+    
+    if (this.isMobile()) {
+        this.getVersionNumber().then(version => {
+            if (this.isAndroid()) {
+                this.appUpdate.checkAppUpdate(updateUrl).then(data => {});
+            } else {
+                alert('ios update');
+            } 
+        });
+    }
+  }
+
+  isMobile(): boolean {
+    return this.platform.is('mobile');
+  }
+
+  isAndroid(): boolean {
+      return this.isMobile() && this.platform.is('android');
+  }
+  
+  isIos(): boolean {
+      return this.isMobile() && (this.platform.is('ios') || this.platform.is('ipad') || this.platform.is('iphone'));
+  }
+}
